b5a8c88d1c3e767be758af28ad8b4af5324251c0
unknown <piotrmlodz@gmail.com>
2023-08-26 16:43:59 +0200

    App.js cleaned

--- src/App.js
+++ src/App.js
@@ -19,25 +19,3 @@ function App() {
 }
 
 export default App;
-
-/*
-
-W tym projekcie znajdziecie wszystkie możliwe widoki aplikacji (w nawiasach podano Routing - pod jakim adresem powinna się znaleźć dana strona):
-
-Home (/)
-Home – Organizations (/)
-Home – Local (/)
-Home – Logged In (/)
-Form - Step 1 (/oddaj-rzeczy)
-Form - Step 2 (/oddaj-rzeczy)
-Form - Step 2 - Opened Select (/oddaj-rzeczy)
-Form - Step 3 (/oddaj-rzeczy)
-Form - Step 3 - Opened Select (/oddaj-rzeczy)
-Form - Step 4 (/oddaj-rzeczy)
-Form - Summary (/oddaj-rzeczy)
-Form - Thank You (/oddaj-rzeczy)
-Login (/logowanie)
-Register (/rejestracja)
-Logout (/wylogowano)
-
-    */
