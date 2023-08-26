1de89bd63f0920c553ea24f68417dca8606390de
unknown <piotrmlodz@gmail.com>
2023-08-26 14:05:55 +0200

    basic configuration, created App.js, index.js, src folders structure, with fonts, colors, main and reset scss

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+function App() {
+    return (
+        <></>
+    );
+}
+
+export default App;
+
+/*
+
+W tym projekcie znajdziecie wszystkie możliwe widoki aplikacji (w nawiasach podano Routing - pod jakim adresem powinna się znaleźć dana strona):
+
+Home (/)
+Home – Organizations (/)
+Home – Local (/)
+Home – Logged In (/)
+Form - Step 1 (/oddaj-rzeczy)
+Form - Step 2 (/oddaj-rzeczy)
+Form - Step 2 - Opened Select (/oddaj-rzeczy)
+Form - Step 3 (/oddaj-rzeczy)
+Form - Step 3 - Opened Select (/oddaj-rzeczy)
+Form - Step 4 (/oddaj-rzeczy)
+Form - Summary (/oddaj-rzeczy)
+Form - Thank You (/oddaj-rzeczy)
+Login (/logowanie)
+Register (/rejestracja)
+Logout (/wylogowano)
+
+    */
