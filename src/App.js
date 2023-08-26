437cf3fca5256a5fba78375d7345f78d17ca371c
unknown <piotrmlodz@gmail.com>
2023-08-26 16:43:15 +0200

    App.js with all routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "../src/components/home/home"
+import Login from "./components/login/login";
+import Logout from "./components/logout/logout";
+import Register from "./components/register/register";
 
 
 function App() {
@@ -7,6 +10,9 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/logowanie" element={<Login/>}/>
+                <Route path="/rejestracja" element={<Register/>}/>
+                <Route path="/wylogowano" element={<Logout/>}/>
             </Routes>
         </BrowserRouter>
     );
